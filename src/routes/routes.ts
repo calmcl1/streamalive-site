ff3b9d9f0be859fdedb29a1f3117e140e8daa97c
Cal McLean <calmcl1@aol.com>
2021-01-01 20:00:15 +0000

    add signup form

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,6 +1,10 @@
 import { Router } from 'express'
 import axios from 'axios'
+// const { Client } = require('sendgrid/client')
 const r = Router()
+
+// const sg = new Client()
+// sg.setAPIKey(process.env.SENDGRID_KEY)
 
 r.get('/',
     (req, res, next) => {
@@ -9,14 +13,31 @@ r.get('/',
 
 r.post('/',
     (req, res, next) => {
-        axios.post("https://api.sendgrid.com/v3/contactdb/recipients",
-            { email: req.body.email_address },
+
+        // sg.request({
+        //     method:"POST",
+        //     url:"/v3/contactdb/recipients",
+        //     body:[{email:req.body.email_address}]
+        // })
+
+        axios.put("https://api.sendgrid.com/v3/marketing/contacts",
             {
-                headers: { "Authorization": `Bearer ${process.env.SENDGRID_KEY}` }
+                contacts: [{ email: req.body.email_address }],
+                list_ids: [
+                    "9463ee06-8004-4ed8-ba51-853cacc1344f"
+                ],
+            },
+            {
+                headers: { "Authorization": `Bearer ${process.env.SENDGRID_KEY}`, "Accept": "application/json", "Content-Type": "application/json" }
             })
             .then(() => { res.render("index", { has_signed_up: true }) })
             .catch(e => {
-                if (e.response) { console.warn(`Email signup failed with code: ${e.response.status}`) }
+                console.log(`Bearer ${process.env.SENDGRID_KEY}`)
+                if (e.response) {
+                    console.warn(`Email signup failed with code: ${e.response.status}`)
+                    console.log(e.response.data)
+                }
+
                 else { console.error(e) }
             })
 
